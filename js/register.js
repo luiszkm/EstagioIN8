baec2b16370af7140eed617c1aae030c36c15294
Luis Murilo <luiszkm@gmail.com>
2022-07-10 01:03:43 -0300

    create register

---
+++ js/register.js
@@ -0,0 +1,50 @@
+
+export function Register() {
+
+  const loadUser = JSON.parse(localStorage.getItem('@register-list'))
+  const users = loadUser || []
+
+  
+
+  let name = document.querySelector('#name')
+  let email = document.querySelector('#email')
+  let date = document.querySelector('#birth')
+  let phone = document.querySelector('#phone')
+
+  const register = document.querySelector('#button-register')
+    .onclick = addUser
+
+  function addUser(event) {
+    event.preventDefault()
+
+    const userObject = {
+      name: name.value,
+      email: email.value,
+      date: date.value,
+      phone: phone.value,
+    }
+    users.push(userObject)
+    localStorage.setItem('@register-list', JSON.stringify(users))
+    clean()
+    alert("Usuário cadastrado com sucesso")
+
+    load.loadUsers()
+    loadMobile.loadUsersTab()
+    
+    name.focus()
+  }
+
+  function clean() {
+    name.value = ""
+    email.value = ""
+    date.value = ""
+    phone.value = ""
+  }
+
+
+
+
+  return{
+    users
+  }
+}
